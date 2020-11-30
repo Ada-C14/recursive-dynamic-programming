35ac616caea32aa4a738096050e0803062dd5737
Li Dai <li.lea.dai@gmail.com>
2020-11-30 01:37:02 -0800

    refactored super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -4,11 +4,12 @@
 # Space Complexity - ?
 
 def super_digit(n)
-  n = super_digit_helper(n, n.dup) if super_digit_helper(n, n.dup) > 9
-  super_digit_helper(n, n.dup)
+  n = super_digit_helper(n) if super_digit_helper(n) > 9
+  super_digit_helper(n)
 end
 
-def super_digit_helper(n, sum)
+def super_digit_helper(n)
+  sum = n
   return sum if n < 10
   sum = n % 10 + super_digit(n / 10)
 end
