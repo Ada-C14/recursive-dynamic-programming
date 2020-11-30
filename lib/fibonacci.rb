8bb4c5623396f6f3a108d4912b5308b14faf8f0e
Li Dai <li.lea.dai@gmail.com>
2020-11-30 01:00:00 -0800

    finished fib

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,19 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n) because we only calculate each fib once
+# Space Complexity - O(n) - call stack memory, pre_two only has two elements, thus it only takes O(1) space. (should be O(n))
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  return fib_helper([0, 1], 2, n)
+end
+
+def fib_helper(pre_two, current, n)
+  raise ArgumentError if n < 0
+  return n if n == 0 || n == 1
+
+  return pre_two.sum if current == n
+
+  pre_two = [pre_two[1], pre_two.sum]
+  return fib_helper(pre_two, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -2,14 +2,25 @@
 
 # Time Complexity - ?
 # Space Complexity - ?
+
 def super_digit(n)
-   
+  return n if n < 10
+  temp = n % 10 + super_digit(n / 10)
+  p  temp
+  # n = temp if n < 10
+
 end
-  
+
+#
+# def super_digit_helper(sum, n)
+#
+# end
+
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
-    
+# def refined_super_digit(n, k)
+#
+# end
+#
+
