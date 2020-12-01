7f50fbae0e8ca23738c60977d274cc51e3592199
Li Dai <li.lea.dai@gmail.com>
2020-11-30 21:32:29 -0800

    refactored pre_two from array to two elements

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,19 +1,21 @@
 # Improved Fibonacci
 
 # Time Complexity - O(n) because we only calculate each fib once
-# Space Complexity - O(n) - call stack memory, pre_two only has two elements, thus it only takes O(1) space. (should be O(n))
+# Space Complexity - O(n) - call stack memory, created only two integer variables, which take O(1) space. (should be O(n))
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  return fib_helper([0, 1], 2, n)
+  return fib_helper(0, 1, 2, n)
 end
 
-def fib_helper(pre_two, current, n)
+def fib_helper(pre_one, pre_two, current, n)
   raise ArgumentError if n < 0
   return n if n == 0 || n == 1
 
-  return pre_two.sum if current == n
+  return pre_one + pre_two if current == n
 
-  pre_two = [pre_two[1], pre_two.sum]
-  return fib_helper(pre_two, current + 1, n)
+  temp = pre_one
+  pre_one = pre_two
+  pre_two += temp
+  return fib_helper(pre_one, pre_two, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -2,7 +2,7 @@
 
 # Time Complexity - O(n)? - We made the recursive call 6 times to get to the answer for 9875. When the sum of the digits (sum) is less than 10, we will call the number of digits times (n). When 100 > sum > 10, we make n + 2 calls. When 1000 > sum > 100, we make n + 3 calls. Because of recursion, we need to run n times again to add up the digits. So it's O(2n) + something like O(log n) maybe??? and thus equals to O(n)?
 # Space Complexity - O(n) - call stack memory
-# I got this solution when refactoring my previous very confusing solution. I don't feel like it's dynamic programming, but it is pretty clean. Basically, I had super_digit(9 / 10) = 0 here. I did try to make a variable sum to save to sum of the current digits, but didn't get it to work.
+# I got this solution when refactoring my previous very chaotic solution. I don't feel like it's dynamic programming, but it seems clean. Basically, I had super_digit(9 / 10) = 0 here. I did try to make a variable sum to save to sum of the current digits, but didn't get it to work. Somehow it kept getting overwritten by n.
 # super_digit(9875)
 # 9875 % 10 + super_digit(9875 / 10) = 5 + 7 + 8 + 9 = 29
 #              = 987 % 10 + super_digit(987 / 10) = 7 + 8 + 9
@@ -24,7 +24,7 @@ end
 
 # Time Complexity -  O(n)??  We will do the O(n) steps as the previous question to go through all the digits for once. Then we will have the extra steps when our sum * k is greater than 9. I guess it depends on how large k is. If k > 10, sum * k is definitely greater than 9 and so on for k > 100, k > 1000. It's not linear, so I am guessing O(log n).
 # Space Complexity - O(n) call stack memory
-# Feel kinda off to use a while loop in a recursion, but if statement does not work no matter what I tried. Stop at 12 for k = 3 and n = 148. Not sure why it's not recursive like the previous one. I feel like converting the number to a string will be easier, but I didn't want the extra space.
+# Feel kinda off to use a while loop in a recursion, but if statement does not work no matter what I tried. Stop at 12 for k = 3 and n = 148. Not sure why it's not recursive like the previous one. I feel like converting the number to a string will be easier for this two problems, but I didn't want the extra space.
 def refined_super_digit(n, k)
   while super_digit_helper(n) * k > 9
     n = super_digit_helper(n) * k
