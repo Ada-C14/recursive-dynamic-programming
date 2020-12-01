bf374614543975e9f266348756cd375bced7c96e
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-01 15:45:04 -0800

    add method for fib

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,17 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n), it goes thru every number less than n, so it's O(n).
+# Space Complexity - O(n), it saves every number less than fib(n), so it's O(n).
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+  return fib_helper([0, 1], 2, n)
+end
+
+def fib_helper(store_array, current, n)
+  return store_array[n] if n < 2
+  store_array.push(store_array[current-1]+store_array[current-2])
+  return store_array[current] if current == n 
+  return fib_helper(store_array, current+1, n)
 end
