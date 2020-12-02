8bd91063af41651a7cb3903e78d8244ca1c46723
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-01 19:36:35 -0800

    add method for super digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,11 +1,21 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n), it depends on the digits of the number, so it's O(n). 
+# Space Complexity - O(1), only use two extra variables, ie sum and result, to do the calculation. So the Space complexity is a constant.
 def super_digit(n)
-   
+  sum = 0
+  result = super_helper(n, sum)
+  while result > 9
+    result = super_helper(result, sum)
+  end
+  return result
 end
-  
+
+def super_helper(n, sum)
+  return sum += n if n < 10
+  sum += n%10
+  return super_helper(n/10, sum)
+end
 
 # Time Complexity - ?
 # Space Complexity - ?
