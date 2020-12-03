b61374256f3be0bedef32ee9119809666e006a5f
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-03 11:10:24 -0800

    define fibonacci and recursive_fib_helper methods

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,29 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
-# Hint, you may want a recursive helper method
-# Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
+# Time Complexity - O(n)
+# The main method is called once and the helper method is called n-1 times.
+# The number of function calls increases linear to an increase in the
+# integer input. All functions performed inside of the helper method
+# are O(1) time complexity so there is no nested complications.
+#
+# Space Complexity - O(n)
+# The number of function calls added to the stack increases linear
+# to the size of the input. The only data structures used in
+# the method are constant (one variable current, which is always
+# an integer, and one array last_two_fib, which is only ever two elements
+# long) and so they do  not add to the space complexity.
+
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+  return 0 if n == 0
+  return 1 if n == 1
+
+  return recursive_fib_helper([0, 1], 2, n)
+end
+
+def recursive_fib_helper(last_two_fib, current, n)
+  new_fib = last_two_fib[0] + last_two_fib[1]
+  return new_fib if current == n
+
+  return recursive_fib_helper([last_two_fib[1], new_fib], current + 1, n)
 end
