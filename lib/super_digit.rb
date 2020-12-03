2f52b50d624a988641cdb05bb34d7ac522a2b456
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-03 11:00:18 -0800

    implement super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,11 +1,22 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(1)
 def super_digit(n)
-   
+  return n if n % 10 == n
+
+  new_sum = super_digit_helper(n, 0)
+  return super_digit(new_sum)
 end
-  
+
+def super_digit_helper(n, memo)
+  dividend, quotient = n.divmod(10)
+  memo += quotient
+
+  n % 10 == n ? memo : super_digit_helper(dividend, memo)
+
+end
+
 
 # Time Complexity - ?
 # Space Complexity - ?
