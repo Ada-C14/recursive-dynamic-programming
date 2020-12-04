4f9efea11daf23e9e12c4dab107dda733b0b557b
Mackenzie <mvlofthus@gmail.com>
2020-12-04 15:38:29 -0800

    super_digit passes, with some puts statements commented out to test, working on time/space complexity

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,10 +1,24 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(log(n))
 def super_digit(n)
-   
+  return n.abs if n.abs < 10
+
+  solution = 0
+  n.abs.to_s.length.times do
+    solution += n % 10
+    n = n/10
+    # puts "n is equal to #{n}"
+    # puts "solution is equal to #{solution}"
+  end
+
+  # puts "sum is equal to #{solution}"
+
+  return super_digit(solution)
 end
+
+# puts super_digit(9875)
   
 
 # Time Complexity - ?
