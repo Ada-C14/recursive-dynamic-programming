277265e89a9963a2a25de8303a60a5bce0e234b4
Mackenzie <mvlofthus@gmail.com>
2020-12-04 15:45:56 -0800

    refined super digit passes

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -6,7 +6,7 @@ def super_digit(n)
   return n.abs if n.abs < 10
 
   solution = 0
-  n.abs.to_s.length.times do
+  until n.zero? do
     solution += n % 10
     n = n/10
     # puts "n is equal to #{n}"
@@ -21,9 +21,25 @@ end
 # puts super_digit(9875)
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(log(n))
 def refined_super_digit(n, k)
-    
+  return n.abs if n.abs < 10
+
+  solution = 0
+  until n.zero? do
+    solution += n % 10
+    n = n/10
+
+    puts "solution is equal to #{solution}"
+    puts "n is equal to #{n}"
+
+  end
+
+  solution *= k
+   puts "sum is equal to #{solution}"
+
+  return refined_super_digit(solution, 1)
 end
-    
+
+puts refined_super_digit(148, 3)
