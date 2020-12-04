3d079f4fbe1cc29a5ac108624c3f6f9a08baa8d5
Jasmine <jasylop@gmail.com>
2020-12-04 15:09:59 -0800

    added super_digit and refined_super_digit methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,13 +3,26 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n if n / 10 == 0
+  count = add_digits(n)
+  return super_digit(count)
+
 end
-  
+
+def add_digits(n)
+  count = 0
+  until n == 0
+    count += n % 10
+    n /= 10
+  end
+  return count
+end
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  return n * k if n * k / 10 == 0
+  count = add_digits(n) * k
+  return refined_super_digit(count, 1)
 end
     
