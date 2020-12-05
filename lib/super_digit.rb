e6ba7e19b82a5552e54f01f9e4fa088075ad000e
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-04 17:42:34 -0800

    super_digit written

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,9 +1,22 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n log m) where n is the value of the passed in integer (and thus determines if the helper will be called... uh idk why I put it as "n" I just just know it is a variable that will determine if the helper method is called, potentially multiple times...), and m is the number of characters of the passed in integer
+# Space Complexity - O(n log m)
 def super_digit(n)
-   
+  if n < 10
+    return n
+  else
+    return super_digit_helper(n, 0)
+  end
+end
+
+def super_digit_helper(n, sum)
+  sum += n % 10
+  if n < 10
+    return super_digit(sum)
+  else
+    return super_digit_helper(n / 10, sum)
+  end
 end
   
 
