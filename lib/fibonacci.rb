ae9d050ad490639db594e345b765babab310fd9f
Iris Lux <iris.lux0@gmail.com>
2020-12-05 01:52:43 -0800

    fib

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,31 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)  (should be O(n))
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
-def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+
+
+def fibonacci(n, i = 2, seq = [0, 1])
+  raise ArgumentError if n < 0
+  return seq[n] if i > n
+  return fibonacci(n, i+1, seq + [seq[i-1] + seq[i-2]])
 end
+
+
+
+
+# loop version
+# def fibonacci(n)
+#   raise ArgumentError if n < 0
+#   seq = []
+#   seq[0], seq[1] = 0, 1
+#
+#   2.upto(n) do |i|
+#     seq[i] = seq[i-1] + seq[i-2]
+#   end
+#
+#   return seq[n]
+# end
+
+
