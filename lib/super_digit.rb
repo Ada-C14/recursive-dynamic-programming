a3ea2f866533070abfb7bfc353a6ea251394afb5
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-04 18:18:29 -0800

    Getting myself confused with time/space complexity...

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,7 +1,7 @@
 # Superdigit
 
-# Time Complexity - O(n log m) where n is the value of the passed in integer (and thus determines if the helper will be called... uh idk why I put it as "n" I just just know it is a variable that will determine if the helper method is called, potentially multiple times...), and m is the number of characters of the passed in integer
-# Space Complexity - O(n log m)
+# Time Complexity - O(m log n) where n is the value of the passed in integer (and thus determines if the helper will be called... uh idk why I put it as "n" I just just know it is a variable that will determine if the helper method is called, potentially multiple times...), and m is the number of characters of the passed in integer
+# Space Complexity - O(m log n)
 def super_digit(n)
   if n < 10
     return n
@@ -20,8 +20,8 @@ def super_digit_helper(n, sum)
 end
   
 
-# Time Complexity - O(n log m) for the same half baked reason as above, but for this algorithm, m is k*m, but since k is some constant, thus an O(1) operation, that can be simplified back to O(n log m).
-# Space Complexity - O(n log m)
+# Time Complexity - O(m log n) for the same half baked reason as above, but for this algorithm, m is k*m, but since k is some constant, thus an O(1) operation, that can be simplified back to O(m log n).
+# Space Complexity - O(m log n)
 def refined_super_digit(n, k)
   n = (n.to_s * k).to_i   #I have no idea what the time/space complexity of a .to_s and then a .to_i operation are.
   if n < 10
