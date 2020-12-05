2a36d6d93345e3caafcc724847617d97fb443198
Emily Nagai <emnagai89@gmail.com>
2020-12-04 18:35:02 -0800

    superdigit first draft

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,8 +3,13 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+
+  #i feel like i'm cheating here. I know that .digits creates a new array...wondering how
+  # this will affect the time/space complexities..o(n^2)?
+  n < 10 ?  n : super_digit(n.digits.sum)
+
 end
+
   
 
 # Time Complexity - ?
