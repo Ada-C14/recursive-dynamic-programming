ea9df185bc42a38a61f1302d424c4844b805afe7
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-04 18:11:51 -0800

    refined_super_digit written

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -20,9 +20,14 @@ def super_digit_helper(n, sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n log m) for the same half baked reason as above, but for this algorithm, m is k*m, but since k is some constant, thus an O(1) operation, that can be simplified back to O(n log m).
+# Space Complexity - O(n log m)
 def refined_super_digit(n, k)
-    
+  n = (n.to_s * k).to_i   #I have no idea what the time/space complexity of a .to_s and then a .to_i operation are.
+  if n < 10
+    return n
+  else
+    return super_digit_helper(n, 0)
+  end
 end
     
