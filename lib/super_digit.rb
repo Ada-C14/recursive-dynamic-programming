931b82097a1ef1f085d81583e22bea10292cacf8
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-05 14:57:24 -0800

    refactor refined_super_digit and add complexity

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,64 +1,72 @@
-# Superdigit
-
-# TODO Ringo in what way in this dynamic programming and not just regular recursion?
+# Time Complexity: O(n^2)
+# The number of times super_digit will be called is
+# determined by the number of digits in the input
+# and it will increase as the number of digits increases.
+# Each time that super_digit is called, it calls helper_digits
+# which runs functions with O(n) time complexity (one after the other).
+# O(n) is nested in O(n) results in O(n^2) time complexity.
 #
-# Time Complexity - O(n log n)
-# The number of times that super_digit will be called
-# does increase as the size of n increases, but in a
-# logarithmic rather than linear fashion, since the controlling
-# factor is the number of digits in n. N of 5 digits will drop to
-# 2 digits after the first pass, and so will n of 6 digits.
-# However, each time that super_digit is called, it runs functions
-# with O(n) time complexity (one after the other, rather than nested).
-# O(n) nested in O(log n) results in O(n log n) time complexity.
+# Space Complexity: O(n)
+# The super_digit calls, being recursive, will stay on the stack trace
+# until all the ones called after them have been resolved. So at most,
+# there will be a number of super_digit calls on the stack at one
+# point in time that is determined by O(n). super_digits creates
+# no data structures, just holds a constant number of integers (n and sum).
 #
-# Space Complexity - O(n)
-# TODO Ringo clear this up
-# At the most things on the stack trace, there will be
-# some number of super_digit calls (determined by but not equal to n),
-# and on top of that all of the calls made by, like, .map.
-# so O(n) + O(log n), in which case, the larger one takes precedence?
-# TODO lol seriously clear this up
-# But also super_digit does create new data structures ever call
-# (string and array), the length of which is determined by (but not equal to)
-# n. But those only last during that one particular function call.
-# 1. do we account for those in the space complexity at *all*,
-# given then only exist for one super_digits call
-# 2. if we do have to factor them in, in this particular instance,
-# are they O(n) or O(log n)? (the array will only get longer as n
-# gets bigger pretty slowly)
-# 3. if we factor them in, do they make a difference? is this O(n) +
-# O(whatever) or nested O(whatever)?
+# Each super_digit calls helper_digits, which makes O(n) function
+# calls. However, each function call for helper_digit is resolved and
+# removed from the stack trace before the next super_digit call, so there
+# is only ever one helper_digits call on the stack at one time. Likewise,
+# helper_digits creates data structures that will increase linear to the
+# size of the input, but these data structures only exist for the length
+# of the helper_digits call.
+#
+# At most, there are O(n) space taken up by super_digits plus O(n)
+# taken up by one function call of helper_digits. O(n) + O(n)
+# results in O(2n), or after dropping the constant, O(n).
 def super_digit(n)
   return n if n < 10
 
-  sum = helper_digits(n).sum
+  sum = helper_digits(n)
   return super_digit(sum)
 end
 
+
 def helper_digits(n)
-  return n.to_s.split("").map { |dig| dig.to_i }
+  digits = n.to_s.split("").map { |dig| dig.to_i }
+  return digits.sum
 end
-  
 
-# Time Complexity - O(n log n)
-# essentially the same logic as the super_digit method
-# TODO because that logic is so solid, ha
-# refined_super_digit gets called once and then essentially
-# treats super_digit as a helper method
-# so we're adding two additional function calls on to the
-# super_digits time complexity but that's a constant and can be
-# pretty much ignored.
+
+# Time complexity: O(n^2)
+# refined_super_digit gets called once.
+# It calls super_digit once or potentially twice, sequentially,
+# so super_digit's O(n^2) time complexity is nested inside of
+# its O(1) time complexity for O(n^2) complexity.
 #
-# Space Complexity - ?
-# TODO
-# whatever the other one is, probably
-# we're adding one additional round of function calls
-# and one additional round of making string and array
-# but that's pretty much it
+# Space complexity: O(n)
+# refined_super_digit adds one stack frame to the call
+# stack. It calls super_digit, which has a space complexity
+# of O(n). refined_super_digit does not build any
+# data structures, just tracks a constant number of integers.
 def refined_super_digit(n, k)
-  return n * k if n * k < 10
+  super_digit = super_digit(n)
+  refined = super_digit * k
 
-  sum = helper_digits(n).sum
-  return super_digit(sum * k)
+  if refined < 10
+    return refined
+  else
+    return super_digit(refined)
+  end
 end
+
+
+# not sure this is dynamic but I liked it anyway
+# it's neater
+#
+# def refined_super_digit(n, k)
+#   return n * k if n * k < 10
+#
+#   sum = helper_digits(n)
+#   return super_digit(sum * k)
+# end
