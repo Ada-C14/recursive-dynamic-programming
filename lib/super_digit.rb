ce38007404e7abf8ee2c626f5902dfd6823bd232
Mackenzie <mvlofthus@gmail.com>
2020-12-05 19:51:33 -0800

    helper methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -2,44 +2,56 @@
 
 # Time Complexity - O(n)
 # Space Complexity - O(log(n))
+
+def digit_helper(n)
+  sum = 0
+  until n.zero? do
+    sum += n % 10
+    n = n/10
+  end
+
+  return sum
+end
+
+def recursive_digit_helper(n)
+  return n if n < 10
+
+  #1489875
+  # 5 = n % 10
+  # n % 10 + recursive_digit_helper(n/10)
+  #
+  return n % 10 + recursive_digit_helper(n / 10)
+end
+
 def super_digit(n)
   return n.abs if n.abs < 10
 
-  solution = 0
-  until n.zero? do
-    solution += n % 10
-    n = n/10
-    # puts "n is equal to #{n}"
-    # puts "solution is equal to #{solution}"
-  end
+  sum = digit_helper(n)
 
-  # puts "sum is equal to #{solution}"
 
-  return super_digit(solution)
+  return super_digit(sum)
 end
 
-# puts super_digit(9875)
+
   
 
-# Time Complexity - O(n)
+# Time Complexity - O(log(n))
 # Space Complexity - O(log(n))
 def refined_super_digit(n, k)
-  return n.abs if n.abs < 10
+  return n.abs if n.abs < 10 && k == 1
+  return 0 if k == 0
 
-  solution = 0
-  until n.zero? do
-    solution += n % 10
-    n = n/10
+  sum = recursive_digit_helper(n)
 
-    puts "solution is equal to #{solution}"
-    puts "n is equal to #{n}"
+  sum *= k
 
-  end
-
-  solution *= k
-   puts "sum is equal to #{solution}"
-
-  return refined_super_digit(solution, 1)
+  return refined_super_digit(sum, 1)
 end
 
-puts refined_super_digit(148, 3)
+puts refined_super_digit(5, 3)
+
+# k= 3 , n= 148148148
+#
+
+
+
