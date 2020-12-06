dbc3092474fa0472a79150fa83ab7b1b79dbf35d
Mackenzie <mvlofthus@gmail.com>
2020-12-05 19:54:08 -0800

    removed comments

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -9,18 +9,14 @@ def fibonacci(n)
   return fib_helper([0,1], 2, n)
 end
 
-
 def fib_helper(solutions, current, n)
   return n if n == 0 || n == 1
-
 
   if current == n
     return solutions[n - 1] + solutions[n - 2]
   end
 
-  # while current < n
     solutions << solutions[current - 1] + solutions[current - 2]
-  # end
 
   return fib_helper(solutions, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,8 +1,5 @@
 # Superdigit
 
-# Time Complexity - O(n)
-# Space Complexity - O(log(n))
-
 def digit_helper(n)
   sum = 0
   until n.zero? do
@@ -16,24 +13,20 @@ end
 def recursive_digit_helper(n)
   return n if n < 10
 
-  #1489875
-  # 5 = n % 10
-  # n % 10 + recursive_digit_helper(n/10)
-  #
   return n % 10 + recursive_digit_helper(n / 10)
 end
+
+# Time Complexity - O(n)
+# Space Complexity - O(log(n))
 
 def super_digit(n)
   return n.abs if n.abs < 10
 
-  sum = digit_helper(n)
-
+  sum = recursive_digit_helper(n)
 
   return super_digit(sum)
 end
 
-
-  
 
 # Time Complexity - O(log(n))
 # Space Complexity - O(log(n))
@@ -42,16 +35,11 @@ def refined_super_digit(n, k)
   return 0 if k == 0
 
   sum = recursive_digit_helper(n)
-
   sum *= k
 
   return refined_super_digit(sum, 1)
 end
 
-puts refined_super_digit(5, 3)
-
-# k= 3 , n= 148148148
-#
 
 
 
