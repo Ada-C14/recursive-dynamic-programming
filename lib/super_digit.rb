9b5ed21cba0e779c3d08acd29e330a2694f730e9
MonaRahmani <monarahmanimehr@gmail.com>
2020-12-05 19:24:25 -0800

    added super digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,20 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
-def super_digit(n)
-   
-end
-  
+# Time Complexity - 0(n)
+# Space Complexity - 0(n)
 
-# Time Complexity - ?
-# Space Complexity - ?
-def refined_super_digit(n, k)
-    
+def super_digit(n)
+  return n if n < 10
+  sum = n.to_s.chars.map(&:to_i).sum
+  return super_digit(sum)
 end
-    
+
+
+# Time Complexity - 0(n)
+# Space Complexity - 0(n)
+def refined_super_digit(n, k)
+  n = (n.to_s * k).to_i
+  super_digit(n)
+end
+
+
