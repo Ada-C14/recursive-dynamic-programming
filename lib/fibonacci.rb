9e9a754a19a7d23010c0d40890425e5413c14f1c
roshni-patel <rpatel01@wesleyan.edu>
2020-12-06 10:46:19 -0600

    Completed improved fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,21 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError, 'Cannot calculate fibonacci for negative integers' if n < 0 
+  return fibonacci_helper(n, 2, [0, 1])
+end
+
+def fibonacci_helper(n, current, solutions)
+  return n if n <= 1
+
+  if n == current
+    return solutions[n - 1] + solutions[n - 2]
+  end
+
+  solutions << solutions[current - 1] + solutions[current - 2]
+  return fibonacci_helper(n, current + 1, solutions)
 end
