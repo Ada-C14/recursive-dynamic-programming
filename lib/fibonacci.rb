b0c8a49a4269367860505c34ca294b36c2b9f3b0
Tram Bui <pbui17@cmc.edu>
2020-12-06 17:53:01 -0600

    recursive for fib

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,20 @@
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  # raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError, 'number can\'t be less than 0' if n < 0
+  return fib_helper([0, 1], 2, n)
 end
+
+def fib_helper(solutions, current, n)
+  return n if n == 0 || n == 1
+
+  if current == n
+    return solutions[n - 1] + solutions[n - 2]
+  end
+
+  solutions << solutions[current - 1] + solutions[current - 2]
+  return fib_helper(solutions, current + 1, n)
+end
+
+p fibonacci(-1)
