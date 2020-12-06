c1b69e36df4cd648006a7d5938723eb0679f9516
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-05 18:30:03 -0800

    add rifined_super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,11 +1,11 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) where n is number of digits
+# Space Complexity - O(n) because of the call stack
 def super_digit(n)
   return n if n / 10 == 0
-  sum = 0
 
+  sum = 0
   until n % 10 == 0
     sum += n % 10
     n /= 10
@@ -15,11 +15,14 @@ def super_digit(n)
   return super_digit(n)
 
 end
-  
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Is this right?  There is definitely a nested method call but it's just to get the initial value
+# Time Complexity - O(n) where n is the number of digits in n
+# Space Complexity - O(n) due to the call stack when calling super
 def refined_super_digit(n, k)
-    
+  # no need to calculate super digit of 148148148
+  # first calculate the super digit of n (148) and then multiply by k
+  # and then call super_digit on that result (39)
+
+  return super_digit(super_digit(n) * k)
 end
-    
