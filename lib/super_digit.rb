9a15a96f2d4d668fdcfe85c4f596b495d90b18d8
roshni-patel <rpatel01@wesleyan.edu>
2020-12-06 14:48:14 -0600

    Completed super digit and super digit refined methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,23 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n < 10
+
+  sum = 0
+  until n == 0
+    sum += n % 10
+    n = n / 10
+  end
+
+  return super_digit(sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - O(n)
 def refined_super_digit(n, k)
-    
+  return super_digit(n * k)
 end
     
