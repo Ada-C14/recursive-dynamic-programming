54efb3a6d32915b14644953de67f9ccbe82cc0b1
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-05 17:51:18 -0800

    fibonacci and super_digit method

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,17 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n if n / 10 == 0
+  sum = 0
+
+  until n % 10 == 0
+    sum += n % 10
+    n /= 10
+  end
+  n = sum
+
+  return super_digit(n)
+
 end
   
 
