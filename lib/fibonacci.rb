924dfd989e2967518702b5a260cb52271ce90bc5
Lina Do <l.do5147@gmail.com>
2020-12-07 10:46:44 -0800

    solved fibonacci problem

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,20 @@
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+  return fib_helper(0, 1, 2, n)
+end
+
+def fib_helper(i, j, current, n)
+
+  return n if n == 0 || n == 1
+
+  return i + j if current == n
+
+  temp = i
+  i = j
+  j = temp + j
+
+  return fib_helper(i, j, current + 1, n)
+
 end
