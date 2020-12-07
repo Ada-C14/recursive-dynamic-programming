77eecf8885553eb6248c630bb57c8750271123b0
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-06 22:44:36 -0500

    all solutions

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,18 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n) - number of cycles correlates directly with size of n
+# Space Complexity - O(n) - array is proportional to n
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+  return 0 if n.zero?
+  helper(n, [0, 1])
 end
+
+def helper(n, array)
+  return array[-1] if n == 1
+  array << array[-1] + array[-2]
+  helper(n - 1, array)
+end
+
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,15 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(1)
 def super_digit(n)
-   
+  return n if n < 10
+  super_digit(n.digits.sum)
 end
-  
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(1) I'm not sure with these space complexities. methods like .split or .digit... are they O(n)?
 def refined_super_digit(n, k)
-    
+  return n if n < 10
+  super_digit(n.digits.sum * k) # k magic hint from https://www.hackerrank.com/challenges/super-digit/forum
 end
-    
