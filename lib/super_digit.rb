39b1bfe2d49abe80d04dad59cb7fa1bdff62640f
OlgaSe <olga.tka4eva@gmail.com>
2020-12-07 18:26:21 -0800

    fibonacci

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,10 +3,23 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
-end
-  
 
+  sum = 0
+  while n > 0 || sum > 9
+    if n == 0
+      n = sum
+      sum = 0
+    end
+    sum += n%10
+    n /= 10
+  end
+  return sum
+end
+
+puts "#{super_digit(30000008)}"
+
+
+#
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
