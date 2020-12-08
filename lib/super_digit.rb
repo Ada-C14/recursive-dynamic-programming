f0fc6d42237919db5c64fa4970b03cd3b619bf74
OlgaSe <olga.tka4eva@gmail.com>
2020-12-07 19:09:26 -0800

    superdigit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,19 +1,18 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2), where n is number of digits
+# Space Complexity - O(n^2), where n is number of digits
 def super_digit(n)
+  return n if n < 10
 
+  current = n
   sum = 0
-  while n > 0 || sum > 9
-    if n == 0
-      n = sum
-      sum = 0
-    end
-    sum += n%10
-    n /= 10
+  while current > 0
+    sum += current%10
+    current /= 10
   end
-  return sum
+
+  return super_digit(sum)
 end
 
 puts "#{super_digit(30000008)}"
@@ -23,6 +22,6 @@ puts "#{super_digit(30000008)}"
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+
 end
-    
+
