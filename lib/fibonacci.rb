53bf11a610e5dce13f9c0c7cad2f212779e40830
Alice Boone <alicegbonel@gmail.com>
2020-12-08 15:46:02 -0800

    Add problems solving

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,9 +1,22 @@
 # Improved Fibonacci
 
-# Time Complexity - ?
-# Space Complexity - ?  (should be O(n))
+# Time Complexity - O(n)
+# Space Complexity - O(n))
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
+
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+  return fib_helper([0, 1], 2, n)
+end
+
+def fib_helper(solution, current, n)
+  return n if n == 0 || n == 1
+
+  if current == n
+    return solution[n - 1] + solution[n -2]
+  end
+
+  solution << solution[current - 1] + solution[current - 2]
+  return fib_helper(solution, current + 1, n)
 end
--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,26 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - 0(n)
 def super_digit(n)
-   
+  return n if n / 10 == 0
+
+    sum_dig = 0
+
+    while n > 0
+      sum_dig = sum_dig + n % 10
+      n = n / 10
+    end
+  super_digit(sum_dig)
 end
+
+p super_digit(9875)
+p super_digit(123)
+p super_digit(5)
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n^2)
+# Space Complexity - 0(n)
 def refined_super_digit(n, k)
-    
+  super_digit(n * k)
 end
-    
