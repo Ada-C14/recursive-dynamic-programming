b78bbfedefb3970ee3825d8845336eb0b9e30ec1
Ida <ida.goitom@yahoo.com>
2020-12-07 17:58:02 -0800

    super_digit method updated and all tests are passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,9 +3,18 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  if n < 10
+    return n
+  end
+
+  super_digit(sum_digits(n))
+
 end
-  
+
+def sum_digits(integer)
+  return digits = integer.to_s.split("").map { |num| num.to_i }.sum
+
+end
 
 # Time Complexity - ?
 # Space Complexity - ?
