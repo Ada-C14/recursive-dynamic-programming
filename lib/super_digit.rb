d584acb7ff0c9a540af74ba8b2895c433a5422dc
Sandy <sandyvasquez9@gmail.com>
2020-12-09 12:54:29 -0800

    fire - sandy

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,32 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity 0(n)
+# Space Complexity 0(n)
 def super_digit(n)
-   
+  sum = 0;
+
+  while n > 0 || sum > 9
+    if n == 0
+      n = sum
+      sum = 0
+    end
+    sum += n % 10
+    n /= 10
+  end
+  return sum
 end
-  
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
-    
+# def refined_super_digit(n, k)
+#
+# end
+#
+
+
+
+
+
+
+
+
