24f2d7a08509fbbbf7a27aef07949a9a12934c69
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-12-08 22:23:19 -0800

    tried to solve the challenges

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,10 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n.digits.sum if n.digits.length <= 1
+
+  return super_digit(n/10).floor
+
 end
   
 
@@ -12,4 +15,5 @@ end
 def refined_super_digit(n, k)
     
 end
-    
+
+p super_digit(9875)
