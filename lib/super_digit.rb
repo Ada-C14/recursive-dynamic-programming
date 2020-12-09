5b4f6b216aa8c4dca9c511afdee1f6cb371f39ed
Hanh Solo <minhhanhnyc@gmail.com>
2020-12-08 21:23:04 -0800

    Improve fibonacci and super_digit

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,22 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - Olog(n)
+# Space Complexity - Olog(n)
+
 def super_digit(n)
-   
+    if n == 1 || n == 0
+        return n
+    end
+    
+    return 1 + ((n - 1) % 9)
+    # return (n % 10 + super_digit(n / 10));
 end
   
+puts super_digit(123)
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
+# def refined_super_digit(n, k)
     
-end
+# end
     
