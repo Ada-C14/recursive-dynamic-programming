13bd70dfa07ed91d029cf2c132d88508412860f5
India Kato <india.kato@gmail.com>
2020-12-09 08:44:52 -0800

    super digit refined method and tests passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -10,9 +10,9 @@ def super_digit(n)
 end
 
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(logn + logk)
+# Space Complexity - O(logn + logk)
 def refined_super_digit(n, k)
-    
+  return super_digit(super_digit(n) * k)
 end
     
