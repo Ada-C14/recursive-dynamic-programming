adbe5f34d5259701898ba961305fc27e1e9fb62a
Sophie Messing <sophie.e.messing@gmail.com>
2020-12-08 18:52:22 -0800

    superdigit passing

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,17 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(log n)
+# Space Complexity - O(n)
 def super_digit(n)
-   
+  return n if n.to_s.length == 1
+  n = n.to_s.split("").map(&:to_i).inject(0) {|sum, x| sum + x}
+  return super_digit(n)
 end
   
 
 # Time Complexity - ?
 # Space Complexity - ?
-def refined_super_digit(n, k)
-    
-end
-    
+# def refined_super_digit(n, k)
+#
+# end
+#
