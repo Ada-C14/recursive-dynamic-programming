2e27db523c35d3105b84821996dab7debcea368d
Madeline <madelign@gmail.com>
2020-12-09 23:11:27 -0500

    finished super_digit and refined_super_digit methods

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,42 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) if n is magnitude of number
+# Space Complexity - O(n)
 def super_digit(n)
-   
+
+  if n < 10
+    return n
+  end
+
+  sum = 0
+  until n == 0
+    p = n % 10
+    n = n / 10
+    sum += p
+  end
+
+  return super_digit(sum)
 end
   
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n)
+# Space Complexity - O(n)
 def refined_super_digit(n, k)
-    
+
+  sum = 0
+  until n == 0
+    p = n % 10
+    n = n / 10
+    sum += p
+  end
+
+  return super_digit(sum * k)
 end
-    
+
+
+# alternative way...?
+# def refined_super_digit(n, k)
+#  concatenate = n.to_s * k
+#
+#   return super_digit(concatenate.to_i)
+# end
