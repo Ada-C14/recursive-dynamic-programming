c51afdd792a967add7037f5f9512cdbd8b434a2e
beauttie <bak02013@pomona.edu>
2020-12-10 00:40:33 -0800

    Implement all functions to pass tests

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -1,15 +1,23 @@
 # Superdigit
 
-# Time Complexity - ?
-# Space Complexity - ?
+# Time Complexity - O(n) where n is number of digits
+# Space Complexity - O(n); n calls on the stack
 def super_digit(n)
-   
+  return n if n < 10
+
+  sum_digits = 0
+  until n == 0
+    sum_digits += n % 10
+    n /= 10
+  end
+
+  super_digit(sum_digits)
 end
   
 
 # Time Complexity - ?
 # Space Complexity - ?
 def refined_super_digit(n, k)
-    
+  return super_digit(k * super_digit(n))
 end
     
