9cbb5c85872a57e5b9c1e56a93a07b7bd4cb659d
dnsrocha <drocha@gmail.com>
2020-12-12 17:21:45 -0800

    added super_digit and sd_helper

--- lib/super_digit.rb
+++ lib/super_digit.rb
@@ -3,7 +3,15 @@
 # Time Complexity - ?
 # Space Complexity - ?
 def super_digit(n)
-   
+  return n if n < 10
+  sum = sd_helper(n)
+
+  super_digit(sum)
+end
+
+def sd_helper(n, m = 0)
+  return m if n == 0
+  sd_helper(n / 10, m + n%10)
 end
   
 
