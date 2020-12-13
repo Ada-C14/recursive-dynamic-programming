f2a46683a30ce7c1a5b70dea491d00c2334480c4
dnsrocha <drocha@gmail.com>
2020-12-12 16:54:07 -0800

    added fibonacci_helper method + fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,18 @@
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  raise NotImplementedError, "Please implement this method for the project"
+  raise ArgumentError if n < 0
+
+  return fibonacci_helper([0, 1], 2, n)
+end
+
+def fibonacci_helper(previous, current, n)
+
+  return n if n == 0 || n == 1
+
+  if current == n
+    return previous[n - 1] + previous[n - 2]
+  end
+
+  return fibonacci_helper(previous, current + 1, n)
 end
