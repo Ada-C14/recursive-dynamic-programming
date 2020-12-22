e49476cfb567c4cf5785e4e3c58fe272ce2731d1
Blaine <b.atsbeha@gmail.com>
2020-12-22 14:33:46 -0800

    finally not broken - fin

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,7 +5,9 @@
 # Hint, you may want a recursive helper method
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
-  # raise NotImplementedError, "Please implement this method for the project"
+  if n < 0 
+    raise ArgumentError, "n must be > 0"
+  end
   if n == 0
     return 0
   end
