1cdfa58a2787a187acc44450c473ba26dd184c18
Blaine <b.atsbeha@gmail.com>
2020-12-21 20:17:02 -0800

    fib has a glitch/don't know why this isn't being recieved by github

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -6,35 +6,27 @@
 # Hint:  Fib(0) = 0, Fib(1) = 1, work up from there
 def fibonacci(n)
   # raise NotImplementedError, "Please implement this method for the project"
-  if n == 0 
+  if n == 0
     return 0
   end
   if n == 1
     return 1
   end
-
 # return fibonacci(n-2) + fibonacci(n-1)
-return fib_helper([0,1] 2, n)
+return fib_helper([0,1], 2, n)
 end
 # Want the 2 previous fib nums returned, 
 def fib_helper(returning_fibs, current_num, n)
-# take in n
-# return 2 fib nums
-# recursive new_n = 2 fib sum
-  if current == n
+  if current_num == n
     return returning_fibs.sum
   end
 # 1 = [0+1]
 # [1, X]
 # [1 , 1]
-
   current_sum = returning_fibs.sum
-  returning_fibs[0] = returning_fib[1]
+  returning_fibs[0] = returning_fibs[1]
   returning_fibs[1] = current_sum
-
 return fib_helper(returning_fibs, current_num + 1, n)
-
-
 end
 # Add two previous to update before then next goes around.
 # Calc sum of the previous 2 numbers then move to the next 2
